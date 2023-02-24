6d8058fd8e381ac750ce8752c7dae5d3bc65d6f7
Grace Yong <grace.yong.xue.min@gmail.com>
2023-02-24 15:21:09 +0800

    access control: senderIsWriter & doctorIsNotPatient

--- contracts/medicalChain.sol
+++ contracts/medicalChain.sol
@@ -2,37 +2,36 @@ pragma solidity ^0.5.0;
 
 contract medicalChain {
     
-
-    struct Record { 
+  struct Record { 
     string cid;
     string fileName; 
     address patientId;
     address doctorId;
     uint256 timeAdded;
-    }
+  }
 
-    struct Patient {
+  struct Patient {
     address id;
     Record[] records;
-    }
+  }
 
-    struct Doctor {
-        address id;
-    }
+  struct Doctor {
+    address id;
+  }
 
-    struct Nurse {
-        address id;
-    }
+  struct Nurse {
+    address id;
+  }
 
   mapping (address => Patient) public patients;
   mapping (address => Doctor) public doctors;
-    mapping (address => Nurse) public nurses;
+  mapping (address => Nurse) public nurses;
 
   event PatientAdded(address patientId);
   event DoctorAdded(address doctorId);
   event NurseAdded(address nurseId);
 
-modifier senderExists {
+  modifier senderExists {
     require(doctors[msg.sender].id == msg.sender|| nurses[msg.sender].id == msg.sender || patients[msg.sender].id == msg.sender, "Sender does not exist");
     _;
   }
@@ -44,6 +43,18 @@ modifier senderExists {
 
   modifier senderIsDoctor {
     require(doctors[msg.sender].id == msg.sender, "Sender is not a doctor");
+    _;
+  }
+
+  // to ensure sender is the one who wrote on the chain (for secondary access)
+  modifier senderIsWriter(Record memory record) {
+    require(record.doctorId == msg.sender, "Sender is not the writer(doctor) of the Record");
+    _;
+  }
+
+  // prevent doctor from self-diagnosing, doctor cannot write on their own record
+  modifier doctorIsNotPatient(address patientId, address doctorId) {
+    require(patientId != doctorId, "Doctor should not self-diagnose");
     _;
   }
 
@@ -79,6 +90,5 @@ modifier senderExists {
       return "unknown";
     }
   }
-
 }
 
