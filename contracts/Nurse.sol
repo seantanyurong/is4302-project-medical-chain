4a6bbb8a76e65656e1cec3e559190702959a3ffb
TohDeKai <tohdekai@outlook.com>
2023-03-13 17:16:41 +0800

    Add Nurse.sol

---
+++ contracts/Nurse.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.0;
+
+import "./EHR.sol";
+
+contract Nurse {
+
+    struct location {
+        string number;
+        string street;
+        string city;
+        string country;
+        string postalCode;
+    }
+
+    struct PublicProfile {
+        string firstName;
+        string lastName;
+        string emailAddress;
+        string dob;
+        location locationAddress;
+        address[] qualifications;
+        string imageUrl;
+    }
+
+    uint256 practitionerId;
+    PublicProfile publicProfile;
+    string[] patients;
+    EHR[] medicalRecords;
+
+}
