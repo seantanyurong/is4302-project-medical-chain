32d1ea251d778f446ef1de8c7ce73a39ceb80db2
nelsonchoo456 <nelsonchoo456@gmail.com>
2023-03-13 21:34:48 +0800

    Added getters and setters for Doctor and Nurse

--- contracts/Doctor.sol
+++ contracts/Doctor.sol
@@ -4,30 +4,98 @@ import "./EHR.sol";
 
 contract Doctor {
 
-    struct location {
-        string number;
-        string street;
-        string city;
-        string country;
-        string postalCode;
-    }
-
-    struct PublicProfile {
-        string firstName;
-        string lastName;
-        string emailAddress;
-        string dob;
-        location locationAddress;
-        address[] qualifications;
-        string imageUrl;
-    }
-
-    uint256 practitionerId;
-    PublicProfile publicProfile;
+    address owner;
+    uint256 doctorId;
+    string firstName;
+    string lastName;
+    string emailAddress;
+    string dob;
+    address[] qualifications;
+ 
     address[] patients;
     EHR[] medicalRecords;
 
+    function getPatients() public view returns (address[] memory) {
+        return patients;
+    }
+
     function addPatient(address patient) public {
         patients.push(patient);
+    }
+
+    function getEHRs() public view returns (EHR[] memory) {
+        return medicalRecords;
+    }
+
+    function addEHR(EHR medicalRecord) public {
+        medicalRecords.push(medicalRecord);
+    }
+
+    // Getters and Setters
+
+    function getDoctorId() public view returns(uint256) {
+        return doctorId;
+    }
+
+    function getFirstName() public view returns(string memory) {
+        return firstName;
+    }
+
+    function setFirstName(string memory _firstName) public {
+        firstName = _firstName;
+    }
+
+    function getLastName() public view returns(string memory) {
+        return firstName;
+    }
+
+    function setLastName(string memory _lastName) public {
+        lastName = _lastName;
+    }
+
+    function getEmailAddress() public view returns(string memory) {
+        return firstName;
+    }
+
+    function setEmailAddress(string memory _emailAddress) public {
+        emailAddress = _emailAddress;
+    }
+
+    function getDob() public view returns(string memory) {
+        return firstName;
+    }
+
+    function setDob(string memory _dob) public {
+        dob = _dob;
+    }
+
+    function getQualifications() public view returns (address[] memory) {
+        return qualifications;
+    }
+
+    function addQualification(address _qualification) public {
+        qualifications.push(_qualification);
+    }
+
+    // Updating
+
+    function updateFirstName(string memory newFirstName) public {
+        require(msg.sender == owner, "This doctor record does not belong to you!");
+        setFirstName(newFirstName);
+    }
+
+    function updateLastName(string memory newLastName) public {
+        require(msg.sender == owner, "This doctor record does not belong to you!");
+        setLastName(newLastName);
+    }
+
+    function updateEmailAddress(string memory newEmailAddress) public {
+        require(msg.sender == owner, "This doctor record does not belong to you!");
+        setEmailAddress(newEmailAddress);
+    }
+
+    function updateDOB(string memory newDOB) public {
+        require(msg.sender == owner, "This doctor record does not belong to you!");
+        setDob(newDOB);
     }
 }
