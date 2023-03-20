97ddb44ded76ef71e10b830112a33beae82605a3
TohDeKai <tohdekai@outlook.com>
2023-03-20 17:04:19 +0800

    add record attributes to EHR.sol

--- contracts/EHR.sol
+++ contracts/EHR.sol
@@ -2,36 +2,44 @@ pragma solidity ^0.5.0;
 
 contract EHR {
 
-    enum EHRstate { unverified, verified }
+    // enum EHRstate { unverified, verified }
     
     struct ehr {
-        EHRstate state;
-        address patient;
+
+        uint256 recordId;
+        // EHRstate state;
+        string fileName; 
+        address patientAddress;
+        address doctorAddress;
+        uint256 timeAdded;
     }
     
-    // mapping of all the records to their IDs
     uint256 public numEHR = 0;
     mapping(uint256 => ehr) public records;
 
     function add(
-        address patient
+        string memory fileName,
+        address patient,
+        address doctor
     ) public payable returns(uint256) {
         require(msg.value > 0.01 ether, "at least 0.01 ETH is needed"); // not sure if we need this
         
         //new EHR object
         ehr memory newEhr = ehr(
-            EHRstate.unverified,
-            patient
+            numEHR,
+            fileName,
+            patient,
+            doctor,
+            now
         );
         
         uint256 newEhrId = numEHR++;
         records[newEhrId] = newEhr; //commit to state variable
-        return newEhrId;   //return new diceId
+        return newEhrId;   //return new ehrId
     }
 
-    //modifier to ensure a function is callable only by patient   
     modifier patientOnly(uint256 ehrId) {
-        require(records[ehrId].patient == msg.sender);
+        require(records[ehrId].patientAddress == msg.sender);
         _;
     }
 
@@ -41,4 +49,7 @@ contract EHR {
         _;
     }
 
+    function getDoctorAddress(uint256 recordId) public view returns(address) {
+        return records[recordId].doctorAddress;
+    }    
 }
